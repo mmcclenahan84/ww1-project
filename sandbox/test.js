8803d7630973c2ccf7d5a5fce5a91dd47b6d29f7
djbpitt <djbpitt@gmail.com>
2022-12-06 11:22:17 -0500

    Add missing semicolon in test.js

--- sandbox/test.js
+++ sandbox/test.js
@@ -1,7 +1,7 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", init);
 function init() {
-  const buttons = document.getElementsByTagName('button')
+  const buttons = document.getElementsByTagName('button');
   for (var i = 0, len = buttons.length; i < len; i++) {
     buttons[i].addEventListener('click', stuff, false);
   }
