68292c478bd59685150c4079b0302699756242de
Ben Adams <bma54@pitt.edu>
2022-12-07 14:55:31 -0800

    updated javascript to add event listeners, needs way to count to 49

--- Website_files/website_js.js
+++ Website_files/website_js.js
@@ -1,17 +1,30 @@
-"use strict";
-window.addEventListener('DOMContentLoaded', init);
-function init(){
-    const dropdowns = document.getElementsByClassName('dd');
-    for (var i = 0, len = dd.length ; i < len; i++){
-        dd[i].addEventListener('click', changeContent, false)
+
+
+function init() {
+    console.log("init");
+    let dropdowns = document.getElementsByClassName('dropdown-content');
+    let childrenOfDropdown;
+    for (let i = 0; i < dropdowns.length; i++) {
+        childrenOfDropdown = dropdowns[i].children;
+        for (let j = 0; j < childrenOfDropdown.length; j++) {
+            childrenOfDropdown[j].addEventListener('click', function () {
+                changeContent(
+                "letter" + ((i+1)*(j+1)))
+                console.log(i);
+                console.log(j);
+            });
+        }
     }
-}
-function changeContent(id){
-   let show_hide = document.getElementsByClassName('special_container')
-    for (i = 0; i < show_hide.length; i++) {
-        show_hide[i].style.display = "none";
-        console.log("second");
-    }
-    [document.getElementsById(id)].style.display = "block";
 }
 
+function changeContent(id) {
+    let show_hide = document.getElementsByClassName('special_container')
+    for (i = 0; i < show_hide.length; i++) {
+        show_hide[i].style.display = "none";
+    }
+    console.log(id);
+    document.getElementById(id).style.display = "block";
+}
+
+window.addEventListener('DOMContentLoaded', init, false);
+console.log("bootstrapping event");
